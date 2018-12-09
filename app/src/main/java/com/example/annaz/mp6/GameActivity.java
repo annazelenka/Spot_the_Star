509269e9878fabe902e8432dd2d2f85f2157e0ba
Shivani Ghanta <shivani.oghanta@gmail.com>
2018-12-09 17:35:59 -0600

    finished timer.

--- app/src/main/java/com/example/annaz/mp6/GameActivity.java
+++ app/src/main/java/com/example/annaz/mp6/GameActivity.java
@@ -1,22 +1,78 @@
 package com.example.annaz.mp6;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.content.Intent;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class GameActivity extends AppCompatActivity {
+    private TextView time;
+    private TextView textViewCountDown;
+    private static long COUNTDOWN_IN_MILLIS = 30000;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMillis;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        textViewCountDown = findViewById(R.id.textView2);
+        timeLeftInMillis = COUNTDOWN_IN_MILLIS;
+        startCountDown();
     }
+
+    private void startCountDown() {
+        countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+
+            }
+
+            @Override
+            public void onFinish() {
+                timeLeftInMillis = 0;
+                updateCountDownText();
+                countDownTimer.cancel();
+
+            }
+        }.start();
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (timeLeftInMillis / 1000) / 60;
+        int seconds = (int) (timeLeftInMillis / 1000) %  60;
+
+        String timeFormatted = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
+
+        textViewCountDown.setText(timeFormatted);
+        textViewCountDown.setTextColor(Color.RED);
+
+    }
+
 
     public void launchScoreScreen(View v) {
         Intent i = new Intent(this, scoreScreen.class);
         startActivity(i);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
 
     }
 }
