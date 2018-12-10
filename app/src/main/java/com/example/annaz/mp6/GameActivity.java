f55cd9825ea3cbefc3d4dfad921cb10846b948dc
AnnaZ <annaz2@illinois.edu>
2018-12-09 19:11:14 -0600

    fix random number and timer now ends the game

--- app/src/main/java/com/example/annaz/mp6/GameActivity.java
+++ app/src/main/java/com/example/annaz/mp6/GameActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import android.widget.ImageView;
 import android.graphics.Bitmap;
 import android.widget.TextView;
+import android.widget.EditText;
 
 
 //CountDownTimer stuff
@@ -34,6 +35,12 @@ import org.json.JSONObject;
 
 public class GameActivity extends AppCompatActivity {
 
+    private String[] celebNames = {"Kanye West", "Kylie Jenner", "Kim Kardashian", "Geoff Challen",
+            "Rihanna", "Beyoncé", "Brad Pitt", "Angelina Jolie", "Justin Bieber", "Selena Gomez"};
+
+    private String currentCeleb;
+    private int currentArrValue = 0;
+
     private TextView time;
     private TextView textViewCountDown;
     private static long COUNTDOWN_IN_MILLIS = 30000;
@@ -44,12 +51,17 @@ public class GameActivity extends AppCompatActivity {
     private boolean timerHasEnded;
     private int score;
 
+
+
     public void guessFunction(View v) { //previously titled "launchScoreScreen"
         Intent i = new Intent(this, scoreScreen.class);
         i.putExtra("score", score); //passes score to the next screen
+        startActivity(i);
 
 
-        if (answerIsCorrect("rihanna")) {
+
+
+        if (answerIsCorrect()) {
             String celebURL = getAPIURL("Rihanna"); //make sure it's capitalized and has spaces if necessary!
             startAPICall(celebURL);
             ImageView imageView = findViewById(R.id.celebImageView);
@@ -71,8 +83,17 @@ public class GameActivity extends AppCompatActivity {
 
     }
 
-    public boolean answerIsCorrect(String answer) {
-        return true;
+    public boolean answerIsCorrect() {
+
+        EditText editText = findViewById(R.id.editText);
+
+        String userAnswer = editText.getText().toString().trim().toLowerCase();
+
+        if (currentCeleb != null && currentCeleb.trim().toLowerCase().equals(userAnswer)) {
+            return true;
+        }
+
+        return false;
     }
 
     public String getAPIURL(String celeb) {
@@ -132,14 +153,14 @@ public class GameActivity extends AppCompatActivity {
 
         } catch (Exception e) {
             e.printStackTrace();
-            TextView answerLabel = findViewById(R.id.answerLabel);
+            EditText answerLabel = findViewById(R.id.editText);
             answerLabel.setText("we messed up"); //why doesn't this do anything???
 
         }
     }
 
     void getImageURL(JSONObject object) {
-        TextView answerLabel = findViewById(R.id.answerLabel);
+        EditText answerLabel = findViewById(R.id.editText);
 
         try {
             //the JSONObject that we get from Wikipedia is reallllllllly nested, so I made a bunch
@@ -148,11 +169,11 @@ public class GameActivity extends AppCompatActivity {
             JSONObject pagesObject = queryObject.getJSONObject("pages");
             String pageNumber = pagesObject.names().toString();
             pageNumber = pageNumber.substring(2, pageNumber.length() - 2);
-            answerLabel.setText(pageNumber);
+            //answerLabel.setText(pageNumber);
             JSONObject pageNumberObject = pagesObject.getJSONObject(pageNumber);
             JSONObject originalObject = pageNumberObject.getJSONObject("original");
             String imageURL = originalObject.getString("source");
-            answerLabel.setText(imageURL);
+            //answerLabel.setText(imageURL);
 
             //upload image to image view
             ImageView imageView = findViewById(R.id.celebImageView);
@@ -175,7 +196,7 @@ public class GameActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(Bitmap response) {
                         imageView.setImageBitmap(response);
-                        TextView answerLabel = findViewById(R.id.answerLabel);
+                        TextView answerLabel = findViewById(R.id.editText);
                         //answerLabel.setText("I didn't mess up");
 
                     }
@@ -183,7 +204,7 @@ public class GameActivity extends AppCompatActivity {
                 new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                TextView answerLabel = findViewById(R.id.answerLabel);
+                TextView answerLabel = findViewById(R.id.editText);
                 answerLabel.setText("I messed up");
 
 
@@ -212,12 +233,23 @@ public class GameActivity extends AppCompatActivity {
         //set up a queue for our Volley requests
         requestQueue = Volley.newRequestQueue(this);
 
+
         //Load main layout
         setContentView(R.layout.activity_game);
+
+
+
+
+        //set up count down timer
 
         textViewCountDown = findViewById(R.id.textView2);
         timeLeftInMillis = COUNTDOWN_IN_MILLIS;
         startCountDown();
+
+        //set first celeb
+        currentCeleb = celebNames[0];
+
+
 
 
     }
@@ -232,6 +264,12 @@ public class GameActivity extends AppCompatActivity {
             @Override public void onFinish() {
                 timeLeftInMillis = 0;
                 updateCountDownText();
+
+
+                Intent i = new Intent(GameActivity.this, scoreScreen.class);
+                i.putExtra("score", score); //passes score to the next screen
+                startActivity(i);
+
                 countDownTimer.cancel();
 
             }
